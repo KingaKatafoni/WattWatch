bf3ef4faa38059835641d29a740e2273c03a2fb8
Kinga <kiniafruit@gmail.com>
2026-09-07 15:50:59 +0200

    Add ENTSO-E API integration with XML price parsing

--- src/main/java/com/wattwatch/wattwatch/service/EntsoeService.java
+++ src/main/java/com/wattwatch/wattwatch/service/EntsoeService.java
@@ -1,14 +1,32 @@
 package com.wattwatch.wattwatch.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class EntsoeService {
+
+    @Value("${entsoe.api.token}")
+    private String apiToken;
+
+    private final WebClient webClient = WebClient.create("https://web-api.tp.entsoe.eu/api");
 
     @Scheduled(fixedRate = 360000)
     public List<BigDecimal> getDayaheadPrices(){
@@ -40,22 +58,23 @@ public class EntsoeService {
 
         );
 
-        for (int i = 0; i < dayPrices.size(); i++){
+        for (int i = 0; i < dayPrices.size(); i++) {
             System.out.println("Godzina " + i + ": " + dayPrices.get(i) + "PLN/MWh");
         }
 
         theCheapest3HoursInRow(dayPrices);
+        fetchDayAheadPrices();
         return dayPrices;
     }
 
-    public List<BigDecimal> theCheapest3HoursInRow(List<BigDecimal> allHours){
+    public List<BigDecimal> theCheapest3HoursInRow(List<BigDecimal> allHours) {
         List<BigDecimal> cheapestHours = new ArrayList<>();
         int bestIndex = 0;
         BigDecimal bestSum = allHours.get(0).add(allHours.get(1)).add(allHours.get(2));
         BigDecimal currentSum;
-        for(int i = 0; i <= allHours.size() -3; i++){
-            currentSum = allHours.get(i).add(allHours.get(i+1)).add(allHours.get(i+2));
-            if(currentSum.compareTo(bestSum) < 0){
+        for (int i = 0; i <= allHours.size() - 3; i++) {
+            currentSum = allHours.get(i).add(allHours.get(i + 1)).add(allHours.get(i + 2));
+            if (currentSum.compareTo(bestSum) < 0) {
                 bestSum = currentSum;
                 bestIndex = i;
             }
@@ -64,9 +83,48 @@ public class EntsoeService {
         cheapestHours.add(allHours.get(bestIndex + 1));
         cheapestHours.add(allHours.get(bestIndex + 2));
 
-        for(int i = 0; i < cheapestHours.size(); i++){
-            System.out.println("Najtansza godzina " + (bestIndex + i)  + " cena: " + cheapestHours.get(i));
+        for (int i = 0; i < cheapestHours.size(); i++) {
+            System.out.println("Najtansza godzina " + (bestIndex + i) + " cena: " + cheapestHours.get(i));
         }
         return cheapestHours;
+    }
+
+    public String fetchDayAheadPrices() {
+        String response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("securityToken", apiToken)
+                        .queryParam("documentType", "A44")
+                        .queryParam("in_Domain", "10YPL-AREA-----S")
+                        .queryParam("out_Domain", "10YPL-AREA-----S")
+                        .queryParam("periodStart", "202609070000")
+                        .queryParam("periodEnd", "202609080000")
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        try {
+            List<BigDecimal> prices = parsePrices(response);
+            for (int i = 0; i < prices.size(); i++) {
+                System.out.println("Pozycja " + (i + 1) + ": " + prices.get(i) + " EUR/MWh");
+            }
+        } catch (Exception ex) {
+            System.out.println("Blad parsowania XML: " + ex.getMessage());
+        }
+
+
+        return response;
+    }
+
+    public List<BigDecimal> parsePrices(String xml) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new InputSource(new StringReader(xml)));
+        NodeList points = doc.getElementsByTagName("price.amount");
+
+        List<BigDecimal> prices = new ArrayList<>();
+        for (int i = 0; i < points.getLength(); i++) {
+            prices.add(new BigDecimal(points.item(i).getTextContent()));
+        }
+        return prices;
     }
 }
